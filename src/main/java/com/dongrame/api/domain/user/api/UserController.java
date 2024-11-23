c180a3d061fb4d1ae5a0df3f4cf9e499ccb4b0f9
SongJaeHoonn <thdwogns121@naver.com>
2024-11-23 21:43:03 +0900

    feat: 회원 프로필 사진 수정 구현

--- src/main/java/com/dongrame/api/domain/user/api/UserController.java
+++ src/main/java/com/dongrame/api/domain/user/api/UserController.java
@@ -11,13 +11,16 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/user")
@@ -47,6 +50,15 @@ public class UserController {
     public ApiResponse<UserResponseDto> getUser() {
         UserResponseDto userResponseDto = userService.getCurrentLoginUser();
         return ApiResponse.success(userResponseDto);
+    }
+
+    @Operation(summary = "프로필 사진 수정", description = "내 프로필 사진을 수정합니다.")
+    @PatchMapping(value = "/updateProfileImage", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ApiResponse<String> updateProfileImage(
+            @RequestPart("profileImage") MultipartFile profileImage
+    ) {
+        String imagePath = userService.updateProfileImage(profileImage);
+        return ApiResponse.success(imagePath);
     }
 
     @Operation(summary = "내 정보 수정", description = "정보 수정이 가능합니다.")
